93606b816a43ccd28e22ac693f1a1a2e46922c0c
Drew Carlisle <drew.carlisle@yahoo.com>
2019-05-25 15:43:45 -0700

    Added proper memory management made main more appropriate

--- main.cpp
+++ main.cpp
@@ -24,10 +24,10 @@ const std::unordered_map<std::string, bool> opleftassoc = { //token, precedence 
 
 bool is_num(const std::string& s)
 {
-	for (char c : s) {if (!isdigit(c)) return false;}
+	for (char c : s) { if (!isdigit(c)) return false; }
 }
 
-enum class TokenType {num, op, name, lparen, rparen};
+enum class TokenType { num, op, name, lparen, rparen };
 
 class ParseToken
 {
@@ -36,8 +36,8 @@ public:
 	TokenType tt;
 	int precedence; //meaningless if tt is not TokenType::op
 	bool leftassoc = false;
-	ParseToken(std::string _val, TokenType _tt, int _precedence = 0, bool _leftassoc=0) : val(_val), tt(_tt),precedence(_precedence), leftassoc(_leftassoc) {};
-	
+	ParseToken(std::string _val, TokenType _tt, int _precedence = 0, bool _leftassoc = 0) : val(_val), tt(_tt), precedence(_precedence), leftassoc(_leftassoc) {};
+
 	bool operator<(const ParseToken& pt) { return precedence < pt.precedence; }
 	bool operator>(const ParseToken& pt) { return precedence > pt.precedence; }
 	bool operator>=(const ParseToken& pt) { return precedence >= pt.precedence; }
@@ -53,14 +53,14 @@ public:
 		for (char c : s) {
 			if (opset.count(std::string(1, c)) || c == ')' || c == '(') {
 				if (temp.length() > 0) {
-					if(is_num(temp))
-						data.push(ParseToken(temp,TokenType::num));
+					if (is_num(temp))
+						data.push(ParseToken(temp, TokenType::num));
 					else
 						data.push(ParseToken(temp, TokenType::name));
 
 					temp.clear();
 				}
-				switch (c){
+				switch (c) {
 				case '(':
 					data.push(ParseToken(std::string(1, c), TokenType::lparen));
 					break;
@@ -71,7 +71,7 @@ public:
 					std::string opss = std::string(1, c);
 					data.push(ParseToken(std::string(1, c), TokenType::op, opset.at(opss), opleftassoc.at(opss)));
 				}
-				
+
 			}
 			else {
 				temp.push_back(c);
@@ -115,10 +115,10 @@ std::queue<ParseToken> shunting_yard(TokenQueue tq)
 			break;
 
 		case TokenType::op:
-			while (!op_stk.empty() && ((op_stk.top().tt == TokenType::op || op_stk.top().tt == TokenType::rparen) && 
-				(op_stk.top() > pt || (op_stk.top().precedence == pt.precedence && pt.leftassoc)) )) {
-					out_q.push(op_stk.top());
-					op_stk.pop();
+			while (!op_stk.empty() && ((op_stk.top().tt == TokenType::op || op_stk.top().tt == TokenType::rparen) &&
+				(op_stk.top() > pt || (op_stk.top().precedence == pt.precedence && pt.leftassoc)))) {
+				out_q.push(op_stk.top());
+				op_stk.pop();
 			}
 			op_stk.push(pt);
 			break;
@@ -142,7 +142,7 @@ std::queue<ParseToken> shunting_yard(TokenQueue tq)
 			out_q.push(tq.pop());
 		}
 	}
-	if (tq.empty()){
+	if (tq.empty()) {
 		while (!op_stk.empty()) {
 			out_q.push(op_stk.top());
 			op_stk.pop();
@@ -155,6 +155,7 @@ struct TokenNode {
 	ParseToken data;
 	std::vector<TokenNode*> branches;
 	TokenNode(ParseToken _data, std::vector<TokenNode*> _branches = {}) : data(_data), branches(_branches) {}
+	~TokenNode() { for (TokenNode* tn : branches) { delete tn; } }
 };
 
 struct ParseTree {
@@ -180,6 +181,10 @@ struct ParseTree {
 		}
 		root = pds.top();
 	}
+
+	~ParseTree() {
+		delete root;
+	}
 };
 
 double add(std::vector<double> vec) { return vec[0] + vec[1]; }
@@ -196,7 +201,7 @@ std::unordered_map<std::string, opfunc > mathmap = {
 	{"/", div}
 };
 
-double eval_tree(const TokenNode *tn) {
+double eval_tree(const TokenNode* tn) {
 	if (tn->data.tt == TokenType::op) {
 		if (mathmap.count(tn->data.val)) {
 			std::vector<double> res;
@@ -211,21 +216,18 @@ double eval_tree(const TokenNode *tn) {
 	}
 }
 
-int main() 
+int main()
 {
-	for (int count = 0; count < 1000; count++) {
-		std::string s = "(((3*2)+(20/4)/5)+(17/2))/12";
-		TokenQueue tq(s);
-		std::queue<ParseToken> oq = shunting_yard(tq);
-		ParseTree tt(oq);
-		int i = 0;
-		/*while (!oq.empty()) {
-			std::cout << oq.front().val << " ";
-			oq.pop();
-		}*/
-		//std::cout << std::endl;
-		//std::cout << eval_tree(tt.root) << std::endl;
-		std::cout << eval_tree(tt.root) << std::endl;
+	std::string s = "(((3*2)+(20/4)/5)+(17/2))/12";
+	TokenQueue tq(s);
+	std::queue<ParseToken> oq = shunting_yard(tq);
+	ParseTree tt(oq);
+	int i = 0;
+	while (!oq.empty()) {
+		std::cout << oq.front().val << " ";
+		oq.pop();
 	}
+	std::cout << std::endl;
+	std::cout << eval_tree(tt.root) << std::endl;
 	return 0;
 }
